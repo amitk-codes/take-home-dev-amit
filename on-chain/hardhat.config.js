1354fe1f33e9baaf8add9753e452d5a466b77e20
Amit Kumar <amitkcodes@gmail.com>
2025-04-07 04:43:15 +0530

    some ui changes in the product page and added a sepolia network in hardhat config

--- on-chain/hardhat.config.js
+++ on-chain/hardhat.config.js
@@ -4,12 +4,20 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    hardhat: {},
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY_REQUIRED_FOR_SEPOLIA_DEPLOY
+        ? [process.env.PRIVATE_KEY_REQUIRED_FOR_SEPOLIA_DEPLOY]
+        : [],
+    },
   },
   paths: {
     artifacts: "./artifacts",
     cache: "./cache",
     sources: "./contracts",
-    tests: "./test"
-  }
+    tests: "./test",
+  },
 };
