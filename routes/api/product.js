bf09186dfc8125a6bb9acc1ba50622eeb8c99e31
Amit Kumar <amitkcodes@gmail.com>
2025-04-06 16:24:38 +0530

    registered routes /search and /verify

--- routes/api/product.js
+++ routes/api/product.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const ProductController = require('../../controllers/ProductController');
 
 router.get('/', ProductController.getProducts);
+router.get('/search', ProductController.searchProducts);
 router.get('/:id', ProductController.getProduct);
 
 module.exports = router;
